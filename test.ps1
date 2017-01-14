87d765dd750cf6602b93b94d20339f4977d35bda
Juni Inacio <juniinacio@users.noreply.github.com>
2017-01-14 14:56:31 +0100

    Add code to create artifacts directory

--- test.ps1
+++ test.ps1
@@ -16,4 +16,8 @@ if ($PSBoundParameters.ContainsKey('TestName')) {
     $params.Add('TestName', $TestName)
 }
 
+if (-not (Test-Path -Path "$PSScriptRoot/artifacts" -PathType Container)) {
+    New-Item -Path "$PSScriptRoot/artifacts" -ItemType Directory | Out-Null
+}
+
 Invoke-Pester -Script "$PSScriptRoot/tests" -EnableExit -OutputFile "$PSScriptRoot/artifacts/TestResults.xml" -OutputFormat NUnitXml @params
